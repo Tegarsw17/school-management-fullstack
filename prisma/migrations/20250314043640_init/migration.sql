1d47c9fbe11f2e134b06f42d7389d092854d366d
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2025-03-15 20:18:59 +0700

    add prisma

---
+++ prisma/migrations/20250314043640_init/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - Added the required column `day` to the `Lesson` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `birthday` to the `Student` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `birthday` to the `Teacher` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Lesson" ADD COLUMN     "day" "Day" NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Student" ADD COLUMN     "birthday" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Teacher" ADD COLUMN     "birthday" TIMESTAMP(3) NOT NULL;
